4e5b510fd7eab690516162492b082d03a8fb6afb
andreikarpiyevich <andreskapi@gmail.com>
2014-12-03 12:59:55 +0300

    time.c

---
+++ time/time.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+	int hh1 = 9, mm1 = 15, ss1 = 10;
+	int hh2 = 15, mm2 = 0, ss2 = 10;
+	int hh3 = 1, mm3 = 0, ss3 = 0;
+
+	printf("%02d:%02d:%02d\n", hh1, mm1, ss1);
+	printf("%02d:%02d:%02d\n", hh2, mm2, ss2);
+	printf("%02d:%02d:%02d\n", hh3, mm3, ss3);
+
+	return 0;
+}
