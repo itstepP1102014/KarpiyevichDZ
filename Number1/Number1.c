980b3d7edab1f7cb65a008b3e609366d35105c0e
andreikarpiyeivch <andreskapi@gmail.com>
2014-12-07 21:47:27 +0300

    Number1.c

---
+++ Number1/Number1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+    int n=0;
+    printf("enter number\n");
+    scanf("%d",&number);
+    while (number>n++){
+     printf("%d ",n);
+    }
+    return 0;
+}
